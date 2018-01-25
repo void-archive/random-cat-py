c480658db40e9f73a26f0cc11f79d2f06b1b7306
void <RyanMcKeown82@Gmail.com>
2018-01-25 20:32:13 +0800

    push code \o/

---
+++ index.py
@@ -0,0 +1,25 @@
+import discord
+import asyncio
+import requests
+
+prefix = "test!"
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print('Logged in as')
+        print(self.user.name)
+        print(self.user.id)
+        print('------')
+
+    async def on_message(self, message):
+        # don't respond to ourselves
+        if message.author == self.user:
+            return
+            
+        if message.content.startswith(prefix + 'cat'):
+            response = requests.get('https://random.cat/meow')
+            data = response.json()
+            await message.channel.send(data['file'])
+
+client = MyClient()
+client.run('bot_token')
